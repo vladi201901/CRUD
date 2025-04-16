ca5a0823065b4d6819df72fed093848d65ef34e9
Jeanfranco Dev <jeanfranco1035@gmail.com>
2025-04-16 15:29:54 -0500

    Segundo Avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ListComentarioComponent } from './components/list-comentario/list-comentario.component';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, ListComentarioComponent],
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListComentarioComponent } from './components/list-comentario/list-comentario.component';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:ListComentarioComponent},
+    {path:'agregar', component:AgregarEditarComentarioComponent}
+];
